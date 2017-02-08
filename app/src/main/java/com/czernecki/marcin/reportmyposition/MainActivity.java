4f50c1981b20cabe4100ffd6686a4525e28028bc
DonRatello <marcin@mczernecki.pl>
2017-02-08 21:12:38 +0100

    Basic layout

--- app/src/main/java/com/czernecki/marcin/reportmyposition/MainActivity.java
+++ app/src/main/java/com/czernecki/marcin/reportmyposition/MainActivity.java
@@ -1,20 +1,100 @@
 package com.czernecki.marcin.reportmyposition;
 
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
+
+import java.util.Calendar;
+
+public class MainActivity extends AppCompatActivity implements
+        GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+
+    Button btnOn;
+    Button btnOff;
+    TextView txtStatus;
+    GoogleApiClient googleApiClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Initialize();
+    }
+
+    public void Initialize() {
+        btnOn = (Button) findViewById(R.id.btnOn);
+        btnOff = (Button) findViewById(R.id.btnOff);
+        txtStatus = (TextView) findViewById(R.id.txtStatus);
+
+        // Create an instance of GoogleAPIClient.
+        if (googleApiClient == null) {
+            googleApiClient = new GoogleApiClient.Builder(this)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .addApi(LocationServices.API)
+                    .build();
+        }
     }
 
     public void pickContact(View view) {
         Log.d("ReportMyPosition", "PickContact clicked");
+    }
+
+    public void btnOnClick(View view) {
+        btnOn.setEnabled(false);
+        btnOff.setEnabled(true);
+        SetStatus("Initializing...");
+
+        googleApiClient.connect();
+    }
+
+    public void btnOffClick(View view) {
+        btnOff.setEnabled(false);
+        btnOn.setEnabled(true);
+        SetStatus("OFF");
+
+        googleApiClient.disconnect();
+    }
+
+    public void SetStatus(String message) {
+        Calendar time = Calendar.getInstance();
+        String sTime = time.get(Calendar.HOUR_OF_DAY) + ":" + time.get(Calendar.MINUTE) + ":" + time.get(Calendar.SECOND);
+        txtStatus.setText(sTime + " -> " + message);
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            return;
+        }
+        Location mLastLocation = LocationServices.FusedLocationApi.getLastLocation(googleApiClient);
+        if (mLastLocation != null) {
+            String position = String.valueOf(mLastLocation.getLatitude()) + String.valueOf(mLastLocation.getLongitude());
+            SetStatus(position);
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
     }
 }
